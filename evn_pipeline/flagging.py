b101e4549e194b78e15057bd9302d4cdea26e74a
Benito Marcote <hawky@hawky.kpn>
2022-04-04 22:19:41 +0200

    Adding the new fiels

---
+++ evn_pipeline/flagging.py
@@ -0,0 +1,49 @@
+import os
+from astropy.io import fits
+import casatasks
+from . import casavlbitools
+from . import project
+
+
+
+def apply_apriori_flagging(project: project.Project):
+    """Applies the flags stored in the file associated with the project.
+    """
+    casatasks.flagdata(vis=str(project.msfile), inpfile=str(project.caldir/f"{project.project_name}.flag"),
+                       reason='any', action='apply', flagbackup=False, savepars=False)
+    casatasks.flagmanager(vis=str(project.msfile), mode='save', versionname='apriori_flags',
+                          comment='A-priori flags prior to any calibration.')
+
+
+
+def edge_channels(project: project.Project, edge_fraction: float=0.1):
+    """Flangs the edge channels of each subband according to the specified edge_fraction.
+    For example, 0.1 (default) will imply to flag the 10% of the channels next to the edge of each subband.
+    """
+    edge_channel = int(project.nchannels/(100*edge_fraction))
+    start = str(edge_channel-1)
+    end = str(project.nchannels - edge_channel)
+    spw2flag = f"*:0~{start};{end}~{str(project.nchannels - 1)}"
+    casatasks.flagdata(vis=str(project.msfile), mode='manual', spw=spw2flag, flagbackup=False)
+
+
+def quack(project: project.Project, quack_interval_s: float=5):
+    """Flags the first seconds of each scan, defined by quack_interval (in seconds).
+    """
+    casatasks.flagdata(vis=str(project.msfile), mode='quack', quackinterval=quack_interval_s, flagbackup=False)
+
+
+
+def flag_tfcrop(project: project.Project):
+    """Runs flagdata with the tfcrop option.
+    In principle it should work fine for EVN data as long as the data are already calibrated.
+    """
+    casatasks.flagdata(vis=str(project.Project), mode='tfcrop', datacolumn='corrected',
+                       field=','.join(project.calibrators), ntime='scan', timecutoff=6.0, freqcutoff=5.0,
+                       timefit='line', freqfit='line', flagdimension='freqtime', extendflags=True,
+                       timedevscale=5.0, freqdevscale=5.0, extendpols=False, growaround=False,
+                       action='apply', flagbackup=True, overwrite=True, writeflags=True)
+
+# flagdata(mode='tfcrop') can be used when the data are calibrated (also in EVN in pprinciple).
+# Good results at that stage (it just looks for outliers).
+
---
+++ evn_pipeline/preprocess.py
@@ -0,0 +1,62 @@
+import os
+from astropy.io import fits
+import casatasks
+from . casavlbitools import casa
+from .casavlbitools import fitsidi
+from .project import Project
+
+
+def append_tsys(project: Project):
+    """Appends the system temperature (Tsys) information to the FITS-IDI files.
+    If the information is already present in the files, it will do nothing.
+    Assumes that the Tsys information is available in a {project_name.lower()}.antab file in the same directory.
+    """
+    hdulist = fits.open(project.idi_files[0])
+    if 'SYSTEM_TEMPERATURE' in hdulist:
+        print('TSYS table already present in FITS-IDI files, skipping the append step.')
+    else:
+        print('Appending TSYS, this takes some time.')
+        fitsidi.append_tsys(f"{project.project_name.lower()}.antab", project.idi_files)
+        print('TSYS appended to the FITS-IDI files.')
+
+
+def generate_gain_curve(project: Project):
+    """Converts the gain curve for each antenna into a CASA readable gain curve table.
+    Assumes that the Tsys information is available in a {project_name.lower()}.antab file in the same directory.
+    """
+    gctable = project.caldir / 'EVN.gc'
+    if gctable.exists():
+        print('GC table already created, skipping the generation of a new gain curve.')
+    else:
+        casa.convert_gaincurve(f"{project.project_name.lower()}.antab", str(gctable),
+                               min_elevation=5.0, max_elevation=90.0)
+
+def convert_uvflg(project: Project):
+    """Converts the AIPS uvflag table into a CASA-compatible format.
+    Assumes that the uvflg information is available in a {project_name.lower()}.uvflg file in the same directory.
+    """
+    flagfile = project.caldir / f"{project.project_name.lower()}.flag"
+    if flagfile.exists():
+        print('Flag file already created, skipping the conversion of the uvflg file.')
+    else:
+        fitsidi.convert_flags(f"{project.project_name.lower()}.uvflg", project.idi_files,
+                              outfile=str(flagfile))
+
+
+def importfitsidi2ms(project: Project):
+    """Converts the existing IDI-FITS files into a MS file.
+    """
+    if project.msfile.exists():
+        print("The MS file associated to {project.project_name} already exists. Will not be replaced.")
+    else:
+        casatasks.importfitsidi(fitsidifile=project.idi_files, vis=str(project.msfile), constobsid=True,
+                                scanreindexgap_s=8.0, specframe='GEO')
+
+
+def get_ms_info(project: Project):
+    project.get_ms_info()
+
+
+def listobs(project: Project):
+    project.get_listobs()
+
